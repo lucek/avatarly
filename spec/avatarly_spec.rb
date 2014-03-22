b37181836ede50a9bf928a7ee3561d94b4e863d0
kustosz <marcin.kostrzewa@llp.pl>
2014-03-22 19:46:02 +0100

    add specs specific for different input formats

--- spec/avatarly_spec.rb
+++ spec/avatarly_spec.rb
@@ -3,17 +3,37 @@ require 'spec_helper'
 describe Avatarly do
   describe '.generate_avatar' do
     it 'generates avatar for given email address' do
-      result = image_from_blob(described_class.generate_avatar("hello.world@example.com",
-                                                               background_color: "#000000"))
-      result.should == reference_image(:HW_black_white_32)
+      result = described_class.generate_avatar("hello.world@example.com",
+                                               background_color: "#000000")
+      assert_image_equality(result, reference_image(:HW_black_white_32))
     end
 
     it 'accepts parameters for size and background and font colors' do
-      result = image_from_blob(described_class.generate_avatar("hello.world@example.com",
-                                                               background_color: "#FFFFFF",
-                                                               font_color: "#000000",
-                                                               size: 64))
-      result.should == reference_image(:HW_white_black_64)
+      result = described_class.generate_avatar("hello.world@example.com",
+                                               background_color: "#FFFFFF",
+                                               font_color: "#000000",
+                                               size: 64)
+      assert_image_equality(result, reference_image(:HW_white_black_64))
+    end
+
+    context 'non-email input' do
+      it 'uses first letters of first two space separated words' do
+        result = described_class.generate_avatar("hello World",
+                                                 background_color: "#000000")
+        assert_image_equality(result, reference_image(:HW_black_white_32))
+      end
+
+      it 'falls back to dot-separated words when no spaces in input' do
+        result = described_class.generate_avatar("hello.World",
+                                                 background_color: "#000000")
+        assert_image_equality(result, reference_image(:HW_black_white_32))
+      end
+
+      it 'falls back to single-letter avatar when no dots and spaces found' do
+        result = described_class.generate_avatar("HelloWorld",
+                                                 background_color: "#000000")
+        assert_image_equality(result, reference_image(:H_black_white_32))
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,16 +3,16 @@ require 'avatarly'
 require 'chunky_png'
 
 module AvatarExpectations
-  def image_from_blob(blob)
-    ChunkyPNG::Image.from_blob(blob)
-  end
-
   def resource_file(name)
     File.expand_path("./resources/#{name}", File.dirname(__FILE__))
   end
 
   def reference_image(name)
-    ChunkyPNG::Image.from_file(resource_file("#{name}.png"))
+    ChunkyPNG::Image.from_file(resource_file("#{name}.png")).to_blob
+  end
+
+  def assert_image_equality(first, second)
+    ChunkyPNG::Image.from_blob(first).should == ChunkyPNG::Image.from_blob(second)
   end
 end
 
