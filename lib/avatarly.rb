45fd926919edac095659f1e09a14aada85fca61f
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2014-03-22 21:55:03 +0100

    make private methods private

--- lib/avatarly.rb
+++ lib/avatarly.rb
@@ -12,75 +12,76 @@ class Avatarly
       "#4d3949", "#ff00aa", "#7f0044", "#f20061", "#330007", "#d96c7b"
     ].freeze
 
-  def self.generate_avatar(text, opts={})
-    generate_image(initials(text.to_s).upcase, parse_options(opts)).to_blob
-  end
-
-  def self.root
-    File.expand_path '../..', __FILE__
-  end
-
-  def self.lib
-    File.join root, 'lib'
-  end
-
-  private
-
-  def self.fonts
-    File.join root, 'assets/fonts'
-  end
-
-  def self.generate_image(text, opts)
-    rvg = Magick::RVG.new(opts[:size], opts[:size]).viewbox(0, 0, opts[:size], opts[:size]) do |canvas|
-      canvas.background_fill = opts[:background_color]
+  class << self
+    def generate_avatar(text, opts={})
+      generate_image(initials(text.to_s).upcase, parse_options(opts)).to_blob
     end
 
-    image = rvg.draw
-    image.format = 'png'
+    def root
+      File.expand_path '../..', __FILE__
+    end
 
-    Magick::Draw.new do
-      self.pointsize = opts[:font_size]
-      self.font = opts[:font]
-      self.fill = opts[:font_color]
-      self.gravity = Magick::CenterGravity
-    end.annotate(image, 0, 0, 0, 0, text)
+    def lib
+      File.join root, 'lib'
+    end
 
-    image
-  end
+    private
 
-  def self.initials(text)
-    if text.dup.is_email? # duplicate used due to the fact that is_email? method changes encoding to binary
-      initials_for_separator(text.split("@").first, ".")
-    elsif text.include?(" ")
-      initials_for_separator(text, " ")
-    else
-      initials_for_separator(text, ".")
+    def fonts
+      File.join root, 'assets/fonts'
+    end
+
+    def generate_image(text, opts)
+      rvg = Magick::RVG.new(opts[:size], opts[:size]).viewbox(0, 0, opts[:size], opts[:size]) do |canvas|
+        canvas.background_fill = opts[:background_color]
+      end
+
+      image = rvg.draw
+      image.format = 'png'
+
+      Magick::Draw.new do
+        self.pointsize = opts[:font_size]
+        self.font = opts[:font]
+        self.fill = opts[:font_color]
+        self.gravity = Magick::CenterGravity
+      end.annotate(image, 0, 0, 0, 0, text)
+
+      image
+    end
+
+    def initials(text)
+      if text.dup.is_email? # duplicate used due to the fact that is_email? method changes encoding to binary
+        initials_for_separator(text.split("@").first, ".")
+      elsif text.include?(" ")
+        initials_for_separator(text, " ")
+      else
+        initials_for_separator(text, ".")
+      end
+    end
+
+    def initials_for_separator(text, separator)
+      if text.include?(separator)
+        text = text.split(separator)
+        text[0][0] + text[1][0]
+      else
+        text[0][0]
+      end
+    end
+
+    def default_options
+      { background_color: BACKGROUND_COLORS.sample,
+        font_color: '#FFFFFF',
+        size: 32,
+        font: "#{fonts}/Roboto.ttf" }
+    end
+
+    def parse_options(opts)
+      opts = default_options.merge(opts)
+      opts[:size] = opts[:size].to_i
+      opts[:font] = default_options[:font] unless Pathname(opts[:font]).exist?
+      opts[:font_size] ||= opts[:size] / 2
+      opts[:font_size] = opts[:font_size].to_i
+      opts
     end
   end
-
-  def self.initials_for_separator(text, separator)
-    if text.include?(separator)
-      text = text.split(separator)
-      text[0][0] + text[1][0]
-    else
-      text[0][0]
-    end
-  end
-
-  def self.default_options
-    { background_color: BACKGROUND_COLORS.sample,
-      font_color: '#FFFFFF',
-      size: 32,
-      font: "#{fonts}/Roboto.ttf" }
-  end
-
-  def self.parse_options(opts)
-    opts = default_options.merge(opts)
-    opts[:size] = opts[:size].to_i
-    opts[:font] = default_options[:font] unless Pathname(opts[:font]).exist?
-    opts[:font_size] ||= opts[:size] / 2
-    opts[:font_size] = opts[:font_size].to_i
-    opts
-  end
-
 end
